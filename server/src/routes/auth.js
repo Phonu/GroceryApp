897035fc90abb9255170a110a119433d785b4700
Phonu <50923254+Phonu@users.noreply.github.com>
2025-02-13 18:04:21 +0530

    created all the routes and middleware verifyToken

---
+++ server/src/routes/auth.js
@@ -0,0 +1,16 @@
+import {
+  loginCustomer,
+  loginDeliveryParter,
+  refreshToken,
+  fetchUser,
+} from "../controllers/auth/auth.js";
+import { verifyToken } from "../middleware/auth.js";
+import { updateUser } from "../controllers/tracking/user.js";
+
+export const authRoutes = async (fastify, options) => {
+  fastify.post("/customer/login", loginCustomer);
+  fastify.post("/delivery/login", loginDeliveryParter);
+  fastify.post("/refresh-token", refreshToken);
+  fastify.get("/user", { preHandler: [verifyToken] }, fetchUser);
+  fastify.patch("/user", { preHandler: [verifyToken] }, updateUser);
+};
---
+++ server/src/routes/order.js
@@ -0,0 +1,28 @@
+import {
+  createOrder,
+  confirmOrder,
+  updateOrderStatus,
+  getOrders,
+  getOrderById,
+} from "../controllers/order/order.js";
+import { verifyToken } from "../middleware/auth.js";
+
+export const orderRoutes = async (fastify, options) => {
+  // fastify.get("/user", { preHandler: [verifyToken] }, fetchUser);
+  // here with the help of hook, we are calling the perHandler
+  // preHandler verification required for all the below order apis.
+  fastify.addHook("preHandler", async (request, reply) => {
+    const isAuthenticated = await verifyToken(request, reply);
+    if (!isAuthenticated) {
+      return reply.code(401).send({ message: "Unauthorized" });
+    }
+  });
+
+  // order/:orderId/status
+  // :orderId => passing this as params in url.
+  fastify.post("/order", createOrder);
+  fastify.get("/order", getOrders);
+  fastify.patch("/order/:orderId/status", updateOrderStatus);
+  fastify.post("/order/:orderId/confirm", confirmOrder);
+  fastify.post("/order/:orderId", getOrderById);
+};
