8ed412c9e57242b27af198b81426ffbb9f51267d
Phonu <50923254+Phonu@users.noreply.github.com>
2025-02-13 16:07:39 +0530

    Created user, category and product api

---
+++ server/src/controllers/products/product.js
@@ -0,0 +1,14 @@
+import Product from "../../models/products.js";
+export const getProductByCategoryID = async (req, reply) => {
+  // http://localhost:3000/product?categoryId="DairyProduct"
+  const { categoryId } = req.params;
+  try {
+    console.log("checking getProductByCategoryID", Product);
+    const products = await Product.find({ category: categoryId })
+      .select("-category")
+      .exec();
+    return reply.send(products);
+  } catch (error) {
+    return reply.status(500).send({ message: "An error occured", error });
+  }
+};
